81607b4245573151b089e92290cc81baacc8bc6a
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-06-18 20:28:15 +0200

    commit

--- InheritanceExamples/src/main/java/edu/es/eoi/Main.java
+++ InheritanceExamples/src/main/java/edu/es/eoi/Main.java
@@ -1,34 +1,51 @@
 package edu.es.eoi;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import edu.es.eoi.service.ProductoService;
 
 public class Main {
-	
-	public static Product[] content=new Product[200];
-	public final static Double NUMERO_PI=3.14;
-	
-		
+
+	public static Product[] content = new Product[200];
+	public final static Double NUMERO_PI = 3.14;
+
 	public static void main(String[] args) {
 
-		Product product1 = new Computer(1, "HP", "descripcion","Windows",800);
-		Product product2 = new Television(1, "TV", "descripcion",42,500);
-				
-		ProductoService.printProduct(product1);	
+		Product product1 = new Computer(1, "HP", "descripcion", "Windows", 800);
+		Product product2 = new Television(1, "TV", "descripcion", 42, 500);
+
+		ProductoService.printProduct(product1);
 		ProductoService.printProduct(product2);
-			
-		System.out.println(product1.calculatePrice()+"€");
-		System.out.println(product2.calculatePrice()+"€");
-			
-		//Autoboxing y unboxing
-		int a=0;
-		double numero=5.0;
-		boolean condicion=false;
-	
-		Integer entero= a;
-		int b=entero;
-	
+
+		System.out.println(product1.calculatePrice() + "€");
+		System.out.println(product2.calculatePrice() + "€");
+
+		// Autoboxing y unboxing
+		int a = 0;
+		double numero = 5.0;
+		boolean condicion = false;
+
+		Integer entero = a;
+		int b = entero;
+
 		System.out.println(entero.toString());
-						
+
+		String hola = "hola pepe";
+
+		if (hola.contains("i") || hola.contains("b") || hola.contains("c")) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
+		
+		Pattern pattern= Pattern.compile("[abc]");
+		Matcher matcher= pattern.matcher(hola);		
+		if(matcher.find()) {
+			System.out.println("Si que se encuentra");
+		}
+
 	}
 
 }
